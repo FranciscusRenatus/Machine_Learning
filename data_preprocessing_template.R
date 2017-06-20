5ee73033997f26f0d07a3d99bed75008c6d0c1f7
Franklin Willemen <franklinwillemen@protonmail.com>
2017-06-20 20:39:41 +0200

    Update data_preprocessing_template.R

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -1,16 +1,33 @@
-# Data Preprocessing Template
+#Data Preprocessing
 
-# Importing the dataset
-dataset = read.csv('Data.csv')
+#Importing the dataset
+dataset = read.csv("Data.csv")
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
+#Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN= function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN= function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
+
+#Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'),
+                         labels = c(0, 1))
+
+#Splitting data into test and train set
+# install.packages("caTools")
+# library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[,2:3])
